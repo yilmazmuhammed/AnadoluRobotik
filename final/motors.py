7615f1f6743f5dab9389689ade30369592e08fdd
Muhammed YILMAZ <yilmazmu15@itu.edu.tr>
2020-07-12 16:43:42 +0300

    Aracın hareketleri için gerekli fonksiyonlar
    - Sola dön
    - Sağa dön
    - xy ekseninde hareket et
    
    Ayrıca deneme amaçlı Klavye yardımıyla aracı sürme kodu eklendi

--- final/motors.py
+++ final/motors.py
@@ -1,6 +1,8 @@
+import math
 from time import sleep
 
 import os
+
 os.system("sudo pigpiod")
 import pigpio
 
@@ -32,19 +34,36 @@ class Motor:
                       positive values​make it work forward.
         :return:
         """
+        power *= -1
         power = power * self.power_multiplier
         pi.set_servo_pulsewidth(self.pin, self.middle_point + power)
         pass
 
     def run_clockwise(self, power):
+        """
+        Suyu geriye ittirir. Motor ileriye doğru hareket etmek ister.
+        :param power:
+        :return:
+        """
         if not 0 <= power <= 100:
             raise NotInCorrectRange("Power must be between 0 and 100.")
         return self.change_power(power)
 
     def run_counterclockwise(self, power):
+        """
+        Suyu ileriye ittirir. Motor geriye doğru hareket etmek ister.
+        :param power:
+        :return:
+        """
         if not 0 <= power <= 100:
             raise NotInCorrectRange("Power must be between 0 and 100.")
         return self.change_power(-power)
+
+    def run_bidirectional(self, power):
+        if power >= 0:
+            self.run_clockwise(power)
+        else:
+            self.run_counterclockwise(-power)
 
     def stop(self):
         return self.change_power(0)
@@ -66,26 +85,55 @@ class RovMovement:
         sleep(2)
 
     def go_up(self, power):
+        power_per_motor = power / 4
         for motor in self.z_motors_list:
-            motor.run_clockwise(power)
+            motor.run_clockwise(power_per_motor)
 
     def go_down(self, power):
+        power_per_motor = power / 4
         for motor in self.z_motors_list:
-            motor.run_counterclockwise(power)
+            motor.run_counterclockwise(power_per_motor)
 
     def turn_left(self, power):
-        pass
+        power_per_motor = power / 4
+        self.xy_rf.run_clockwise(power_per_motor)
+        self.xy_lf.run_counterclockwise(power_per_motor)
+        self.xy_lb.run_clockwise(power_per_motor)
+        self.xy_rb.run_counterclockwise(power_per_motor)
 
     def turn_right(self, power):
-        pass
+        power_per_motor = power / 4
+        self.xy_rf.run_counterclockwise(power_per_motor)
+        self.xy_lf.run_clockwise(power_per_motor)
+        self.xy_lb.run_counterclockwise(power_per_motor)
+        self.xy_rb.run_clockwise(power_per_motor)
 
-    def go_xy(self, power, angle):
+    def go_xy(self, power, degree):
         """
         :param power: Power sent to the vehicle's movement
-        :param angle: angle of movement (for positive x-axis)
+        :param degree: degree of movement (0between 0-360 degree)
+                       0 -> ileri
+                       90 -> sağa
+                       180 -> geri
+                       270 -> sola
         :return:
         """
-        pass
+        power_per_motor = power / 4
+
+        radian_rf = (45 - degree) / 180 * math.pi
+        radian_lf = (135 - degree) / 180 * math.pi
+        radian_lb = (225 - degree) / 180 * math.pi
+        radian_rb = (315 - degree) / 180 * math.pi
+
+        pow_rf = int(math.sin(radian_rf) * power_per_motor)
+        pow_lf = int(math.sin(radian_lf) * power_per_motor)
+        pow_lb = int(math.sin(radian_lb) * power_per_motor)
+        pow_rb = int(math.sin(radian_rb) * power_per_motor)
+
+        self.xy_rf.run_bidirectional(pow_rf)
+        self.xy_lf.run_bidirectional(pow_lf)
+        self.xy_lb.run_bidirectional(pow_lb)
+        self.xy_rb.run_bidirectional(pow_rb)
 
     def run_all_motors_cw(self, power):
         for motor in self.all_motors_list:
@@ -108,73 +156,119 @@ if __name__ == '__main__':
     rov_movement = RovMovement(xy_lf_pin=0, xy_rf_pin=0, xy_lb_pin=0, xy_rb_pin=0,
                                z_lf_pin=0, z_rf_pin=0, z_lb_pin=0, z_rb_pin=0)
     try:
-        secenek = input("1: Saat yönünün tersine son hızdan, saat yönünde son hıza\n"
-                        "2: Elle güç gir\n"
-                        "3: Arttır/Azalt\n"
-                        "\nİstediğiniz seçeneği seçiniz: ")
-        if secenek == '1':
-            for pow in range(100, -1, -1):
-                print("pow:", pow)
-                rov_movement.run_all_motors_ccw(pow)
-                sleep(0.5)
-            for pow in range(101):
-                print("pow:", pow)
-                rov_movement.run_all_motors_cw(pow)
-                sleep(0.5)
-        elif secenek == '2':
-            print("Motor yönünü değiştirmek için '+' veya '-' giriniz\n"
-                  "Kapatmak için 'c' giriniz\n"
-                  "Motorun çalışmasını istediğiniz gücü 0-100 aralığında giriniz\n")
-            yon = "+"
-            pow = 0
-            while True:
-                if yon == "+":
-                    print("pow:", pow)
-                    rov_movement.run_all_motors_cw(pow)
-                elif yon == "-":
-                    print("pow:", pow)
-                    rov_movement.run_all_motors_ccw(pow)
-                deger = input()
-                if deger == "+":
-                    yon = "+"
-                elif deger == "-":
-                    yon = "-"
-                elif deger.isnumeric():
-                    pow = int(deger)
-                elif deger == "c":
-                    break
-                else:
-                    print("Your input is '" + deger + "'. Please enter valid input.")
-                    print("Motor yönünü değiştirmek için '+' veya '-' giriniz\n"
-                          "Kapatmak için 'c' giriniz\n"
-                          "Motorun çalışmasını istediğiniz gücü 0-100 aralığında giriniz\n")
-        elif secenek == '3':
-            print("Motor hızını değiştirmek için 'a' veya 'd' giriniz\n"
-                  "Kapatmak için 'c' giriniz\n")
-            pow = 0
-            while True:
-                if pow >= 0:
-                    print("pow:", pow)
-                    rov_movement.run_all_motors_cw(pow)
-                else:
-                    print("pow:", pow)
-                    rov_movement.run_all_motors_ccw(-pow)
+        import getch
 
-                deger = input()
-                if deger == "a":
-                    if pow <= 100:
-                        pow += 10
-                elif deger == "d":
-                    if pow >= -100:
-                        pow -= 10
-                elif deger == "c":
-                    break
-                else:
-                    print("Your input is '" + deger + "'. Please enter valid input.")
-                    print("Motor hızını değiştirmek için 'a' veya 'd' giriniz\n"
-                          "Kapatmak için 'c' giriniz\n")
-        else:
-            print("Your input is '" + secenek + "'. Please enter valid input.")
+        power = 0
+        char = "w"
+        while True:
+            previous = char
+            char = getch.getch()
+            if previous != char:
+                rov_movement.stop()
+
+            if char == "a":
+                print("Hareket: sola git", "\tGüç:", power)
+                rov_movement.go_xy(power, 270)
+            elif char == "s":
+                print("Hareket: geri git", "\tGüç:", power)
+                rov_movement.go_xy(power, 180)
+            elif char == "d":
+                print("Hareket: sağa git", "\tGüç:", power)
+                rov_movement.go_xy(power, 90)
+            elif char == "w":
+                print("Hareket: ileri git", "\tGüç:", power)
+                rov_movement.go_xy(power, 0)
+            elif char == "y":
+                print("Hareket: yüksel", "\tGüç:", power)
+                rov_movement.go_up(power)
+            elif char == "u":
+                print("Hareket: alçal", "\tGüç:", power)
+                rov_movement.go_down(power)
+            elif char == "q":
+                print("Hareket: sola dön", "\tGüç:", power)
+                rov_movement.turn_left(power)
+            elif char == "e":
+                print("Hareket: sağa dön", "\tGüç:", power)
+                rov_movement.turn_right(power)
+            elif char == "j":
+                print("Hareket: güçü arttır", "\tGüç:", power)
+                if power <= 90:
+                    power += 10
+            elif char == "k":
+                print("Hareket: güçü azalt", "\tGüç:", power)
+                if power >= 10:
+                    power -= 10
+            else:
+                rov_movement.stop()
+                print("Tanımsız karaktere basıldı")
+    # try:
+    #     secenek = input("1: Saat yönünün tersine son hızdan, saat yönünde son hıza\n"
+    #                     "2: Elle güç gir\n"
+    #                     "3: Arttır/Azalt\n"
+    #                     "\nİstediğiniz seçeneği seçiniz: ")
+    #     if secenek == '1':
+    #         for pow in range(100, -1, -1):
+    #             print("pow:", pow)
+    #             rov_movement.run_all_motors_ccw(pow)
+    #             sleep(0.5)
+    #         for pow in range(101):
+    #             print("pow:", pow)
+    #             rov_movement.run_all_motors_cw(pow)
+    #             sleep(0.5)
+    #     elif secenek == '2':
+    #         print("Motor yönünü değiştirmek için '+' veya '-' giriniz\n"
+    #               "Kapatmak için 'c' giriniz\n"
+    #               "Motorun çalışmasını istediğiniz gücü 0-100 aralığında giriniz\n")
+    #         yon = "+"
+    #         pow = 0
+    #         while True:
+    #             if yon == "+":
+    #                 print("pow:", pow)
+    #                 rov_movement.run_all_motors_cw(pow)
+    #             elif yon == "-":
+    #                 print("pow:", pow)
+    #                 rov_movement.run_all_motors_ccw(pow)
+    #             deger = input()
+    #             if deger == "+":
+    #                 yon = "+"
+    #             elif deger == "-":
+    #                 yon = "-"
+    #             elif deger.isnumeric():
+    #                 pow = int(deger)
+    #             elif deger == "c":
+    #                 break
+    #             else:
+    #                 print("Your input is '" + deger + "'. Please enter valid input.")
+    #                 print("Motor yönünü değiştirmek için '+' veya '-' giriniz\n"
+    #                       "Kapatmak için 'c' giriniz\n"
+    #                       "Motorun çalışmasını istediğiniz gücü 0-100 aralığında giriniz\n")
+    #     elif secenek == '3':
+    #         print("Motor hızını değiştirmek için 'a' veya 'd' giriniz\n"
+    #               "Kapatmak için 'c' giriniz\n")
+    #         pow = 0
+    #         while True:
+    #             if pow >= 0:
+    #                 print("pow:", pow)
+    #                 rov_movement.run_all_motors_cw(pow)
+    #             else:
+    #                 print("pow:", pow)
+    #                 rov_movement.run_all_motors_ccw(-pow)
+    #
+    #             deger = input()
+    #             if deger == "a":
+    #                 if pow <= 100:
+    #                     pow += 10
+    #             elif deger == "d":
+    #                 if pow >= -100:
+    #                     pow -= 10
+    #             elif deger == "c":
+    #                 break
+    #             else:
+    #                 print("Your input is '" + deger + "'. Please enter valid input.")
+    #                 print("Motor hızını değiştirmek için 'a' veya 'd' giriniz\n"
+    #                       "Kapatmak için 'c' giriniz\n")
+    #     else:
+    #         print("Your input is '" + secenek + "'. Please enter valid input.")
     except KeyboardInterrupt:
         print("KeyboardInterrupt yakalandı")
 
